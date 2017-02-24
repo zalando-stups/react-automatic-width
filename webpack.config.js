9e9cc4aa0f2fd48b5205cb027a3c335f9438b6ce
Nikolaus Piccolotto <prayerslayer@gmail.com>
2017-02-24 14:40:03 +0100

    update to webpack2

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,48 @@
-var webpack = require('webpack');
+var webpack = require("webpack");
 
 module.exports = {
-    devtool: 'eval',
-    entry: [
-        './src/react-automatic-width.jsx'
-    ],
-    output: {
-        path: __dirname + '/dist/',
-        filename: 'react-automatic-width.js',
-        library: 'react-automatic-width',
-        libraryTarget: 'umd'
-    },
-    plugins: [
-        new webpack.BannerPlugin(require('./banner')),
-        new webpack.optimize.DedupePlugin(),
-        new webpack.optimize.UglifyJsPlugin(),
-        new webpack.DefinePlugin({
-          "process.env": {
-            NODE_ENV: JSON.stringify("production")
-          }
-        }),
-    ],
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    eslint: {
-        formatter: require('eslint-formatter-pretty')
-    },
-    externals: {
-        'react': 'react'
-    },
-    module: {
-        preLoaders: [{
-            test: /\.jsx$/,
-            exclude: /node_modules/,
-            loader: 'eslint'
-        }],
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel'
-        }]
-    }
+  devtool: "eval",
+  entry: ["./src/react-automatic-width.jsx"],
+  output: {
+    path: __dirname + "/dist/",
+    filename: "react-automatic-width.js",
+    library: "react-automatic-width",
+    libraryTarget: "umd"
+  },
+  plugins: [
+    new webpack.BannerPlugin(require("./banner")),
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      options: {
+        eslint: {
+          formatter: require("eslint-formatter-pretty")
+        }
+      }
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  externals: {
+    react: "react"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        enforce: "pre",
+        loader: "eslint-loader"
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
